63673dcfa4ff5c048866087ff32e91b28d105ede
Purnima <pehu.verma145@gmail.com>
2020-10-22 11:06:33 -1200

    update

--- school.py
+++ school.py
@@ -29,7 +29,7 @@ def userchoice():
        ****************************************
     1) New Resgistration.
     2)Show existing school file list.
-    3) Enter in school.
+    3)Enter in school.
     4)Exit.
     '''     
 print('\nchoicelist: ')
@@ -46,3 +46,27 @@ elif choice == 2:
     createfile(fileName)    
 elif choice == 3:
     print('\nEnter in school: ')
+else:
+    print('\nexit')
+
+class student():
+    # Constructor 
+    def __init__(self, name, rollno, marks1, marks2): 
+        self.name = name 
+        self.rollno = rollno 
+        self.marks1 = marks1 
+        self.marks2 = marks2 
+        # Function to create and append new student    
+    def accept(self, Name, Rollno, marks1, marks2 ): 
+        # use  ' int(input()) ' method to take input from user 
+        ob = student(Name, Rollno, marks1, marks2 ) 
+        list.append(ob) 
+  
+    # Function to display student details      
+    def display(self, ob): 
+            print("Name   : ", ob.name) 
+            print("RollNo : ", ob.rollno) 
+            print("Marks1 : ", ob.marks1) 
+            print("Marks2 : ", ob.marks2) 
+            print("\n")  
+
